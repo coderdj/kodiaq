a6ac4501b94ae6f9066a2e1960192bffebc02e16
Daniel <dan@xedaq2.(none)>
2013-12-09 18:08:50 +0100

    How can all these files be required?

---
+++ build/config.h
@@ -0,0 +1,108 @@
+/* build/config.h.  Generated from config.h.in by configure.  */
+/* build/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `CAENVME' library (-lCAENVME). */
+/* #undef HAVE_LIBCAENVME */
+
+/* Define to 1 if you have the `DDC' library (-lDDC). */
+/* #undef HAVE_LIBDDC */
+
+/* Define to 1 if you have the `expect' library (-lexpect). */
+/* #undef HAVE_LIBEXPECT */
+
+/* Define to 1 if you have the `form' library (-lform). */
+#define HAVE_LIBFORM 1
+
+/* Define to 1 if you have the `menu' library (-lmenu). */
+#define HAVE_LIBMENU 1
+
+/* Define to 1 if you have the `mongoclient' library (-lmongoclient). */
+/* #undef HAVE_LIBMONGOCLIENT */
+
+/* Define to 1 if you have the `ncurses' library (-lncurses). */
+#define HAVE_LIBNCURSES 1
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+/* #undef HAVE_LIBPTHREAD */
+
+/* Define to 1 if you have the `snappy' library (-lsnappy). */
+/* #undef HAVE_LIBSNAPPY */
+
+/* Define to 1 if you have the `tcl8.5' library (-ltcl8.5). */
+/* #undef HAVE_LIBTCL8_5 */
+
+/* Define to 1 if you have the `tinfo' library (-ltinfo). */
+#define HAVE_LIBTINFO 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "kodiaq"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "daniel.coderre@lhep.unibe.ch"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "kodiaq"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "kodiaq 0.9"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "kodiaq"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.9"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.9"
+
+/* Compile with DDC HE veto support */
+/* #undef WITH_DDC10 */
+
+/* Compile master module */
+/* #undef WITH_MASTER */
+
+/* Compile slave module */
+/* #undef WITH_SLAVE */
+
+/* Compile user interface */
+#define WITH_USER /**/
