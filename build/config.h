7c225b4f15cc97031cc7e31cd406172eb89914a2
Daniel Coderre <coderdj@gmail.com>
2015-04-02 17:10:50 +0200

    Added NCursesUI to common

--- build/config.h
+++ build/config.h
@@ -22,7 +22,7 @@
 #define HAVE_LIBBOOST_THREAD 1
 
 /* Define to 1 if you have the `CAENVME' library (-lCAENVME). */
-#define HAVE_LIBCAENVME 1
+/* #undef HAVE_LIBCAENVME */
 
 /* Define to 1 if you have the `expect' library (-lexpect). */
 /* #undef HAVE_LIBEXPECT */
@@ -31,7 +31,7 @@
 #define HAVE_LIBMONGOCLIENT 1
 
 /* Define to 1 if you have the `ncurses' library (-lncurses). */
-#define HAVE_LIBNCURSES 1
+/* #undef HAVE_LIBNCURSES */
 
 /* Define to 1 if you have the `pbf' library (-lpbf). */
 #define HAVE_LIBPBF 1
@@ -43,7 +43,7 @@
 #define HAVE_LIBPTHREAD 1
 
 /* Define to 1 if you have the `snappy' library (-lsnappy). */
-#define HAVE_LIBSNAPPY 1
+/* #undef HAVE_LIBSNAPPY */
 
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
@@ -70,7 +70,7 @@
 #define HAVE_UNISTD_H 1
 
 /* Compile standalone slave */
-#define KLITE 1
+/* #undef KLITE */
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
@@ -107,7 +107,7 @@
 /* #undef WITH_DDC10 */
 
 /* Compile master module */
-/* #undef WITH_MASTER */
+#define WITH_MASTER /**/
 
 /* Compile slave module */
 /* #undef WITH_SLAVE */
---
+++ src/common/NCursesUI.hh
@@ -0,0 +1,118 @@
+#ifndef _NCURSESUI_HH_
+#define _NCURSESUI_HH_
+/*
+ *
+ * Name:     NCursesUI.hh
+ * Author:   Daniel Coderre, LHEP Uni Bern
+ * Date:     19.02.2015
+ *
+ * Brief:    General NCurses UI
+ *
+*/
+
+#include <ncurses.h>
+#include <menu.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <kbhit.hh>
+#include "koHelper.hh" 
+
+using namespace std;
+
+struct koMasterConfig_t{
+  string filepath;
+  string monitorDB;
+  string monitorAddr;
+  string outputPath;
+  string outputMode;  
+};
+
+class NCursesUI
+{
+public:
+  
+  NCursesUI();
+  virtual ~NCursesUI();
+
+  // Initialize draws the main menu
+  // if standalone is true draw for standalone mode. 
+  // otherwise master mode.
+  // You have to send an argument with the function used to poll data
+  int Initialize( bool standalone,  koMasterConfig_t conf,
+		  koStatusPacket_t (*f)(string), vector<string> detectors );
+
+  // Starts running
+  int StartLoop();
+
+  // Add message  
+  // highlight ( 0=none, 1=blue, 2=yellow, 3=red, 4=green )  
+  int AddMessage( string message, int highlight=0 );
+
+private:
+  
+  // Refresh refreshes the entire screen 
+  void Refresh();
+
+  // DispBar displays a message on the bottom bar
+  // Should be available commands
+  int DrawBottomBar( );
+
+  // Pops up an input box for user input. User input is put in reply
+  int InputBox( string prompt, string &reply );
+
+  // Updates display with text information
+  int UpdateRunDisplay( );
+
+  //int UpdateRunDisplay( int status, double rate, double freq );
+  
+  int DrawOutputBox();
+
+  int DrawMessageWin();
+
+  // update the output mode
+  int UpdateOutput( string mode, string path );
+  
+  // update the memory usage in board buffers. Size in bytes
+  // only for standalone deployment
+  //int UpdateBufferSize( int size );
+
+  int AddStatusPacket( koStatusPacket_t DAQStatus, string detector );
+
+  int AddOutputMode( string detector, string mode, string path );
+  WINDOW* create_newwin(int height, int width, int starty, int startx);
+
+  void Close();
+  int Redraw();
+  int WindowResize();
+  void MoveMessagesUp();
+  void MoveMessagesDown();
+
+  string GetFillString( double rate );
+
+  bool    bStandalone;
+  string  mCurrentDet;
+  WINDOW *STATUS_WIN, *OUTPUT_WIN, *MESSAGE_WIN, *CONTROL_WIN;
+  map<string, int> mMessages; // messages and highlight IDs
+  map<string, koStatusPacket_t> mLatestStatus; // latest status for each det
+  // needs all
+  map<string, time_t> mLatestUpdate; // latest update each det
+  // needs all
+  map<string, string> mOutputModes;
+  // needs all
+
+  // dimensions. Should be easy to change
+  int mSTATUS_HEIGHT, mSTATUS_WIDTH, mCONTROL_HEIGHT, mCONTROL_WIDTH;
+  int mOUTPUT_HEIGHT, mOUTPUT_WIDTH, mMESSAGE_HEIGHT, mMESSAGE_WIDTH;
+  int mSTATUS_INDEX, mOUTPUT_INDEX, mCONTROL_INDEX, mMESSAGE_INDEX;
+  int mSTARTX, mSTARTY;
+
+  int mWINDOW_WIDTH, mWINDOW_HEIGHT; 
+
+  // messages info
+  int mMessageIndex;
+};
+
+#endif
+  
+  
