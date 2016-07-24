bc323432eb403e65c06c5945be877fe0aa8e678f
CyrilGrignon <grignon@uni-mainz.de>
2016-07-24 16:29:33 +0200

    Added possibility to load VME options for unique boards only, once baselines are loaded in Initialize function

--- src/slave/CBV1724.cc
+++ src/slave/CBV1724.cc
@@ -108,7 +108,7 @@ int CBV1724::Initialize(koOptions *options)
 
   // Reload VME options
   int tries = 0;
-  while( LoadVMEOptions( options ) != 0 && tries < 5) {
+  while( LoadVMEOptions( options, false ) != 0 && tries < 5) {
     tries ++;
     usleep(100);
     if( tries == 5 )
@@ -124,6 +124,16 @@ int CBV1724::Initialize(koOptions *options)
   fBufferOccSize = 0;
   fBufferOccCount = 0;
   fReadoutReports.clear();
+
+  // Load user registers for boards with a unique ID only 
+  // this is to avoid user settings to be overwritten
+  tries = 0;
+  while( LoadVMEOptions( options,true ) != 0 && tries < 5) {
+     tries ++;
+     usleep(100);
+     if( tries == 5 )                         
+     retVal = -1;
+  }
 
   // Report whether we succeeded or not
   stringstream messstr;
@@ -722,12 +732,12 @@ int CBV1724::LoadDAC(vector<int> baselines){
   return 0;
 }
 
- int CBV1724::LoadVMEOptions( koOptions *options )
+ int CBV1724::LoadVMEOptions( koOptions *options, bool unique )
  {
    int retVal = 0;
    // Reload VME options    
    for(int x=0;x<options->GetVMEOptions();x++)  {
-     if((options->GetVMEOption(x).board==-1 ||
+     if( ( (options->GetVMEOption(x).board==-1 && unique == false) ||
 	 options->GetVMEOption(x).board==fBID.id)){
        int success = WriteReg32(options->GetVMEOption(x).address,
 				options->GetVMEOption(x).value);
